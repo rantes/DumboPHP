1931ad0a439fe68de05f235afbf059a200334f0f
Javier Serrano <rantes.javier@gmail.com>
2013-03-08 13:46:04 -0500

    changes on views

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
+xhprof_enable();
 include_once '../../config/host.php';
-set_include_path(implode(PATH_SEPARATOR, array(INST_PATH . 'vendors', INST_PATH . 'masturbo', get_include_path(),)));
+set_include_path(implode(PATH_SEPARATOR, array(INST_PATH . 'vendors', INST_PATH . 'masturbo', get_include_path(),PEAR_EXTENSION_DIR)));
 
 
 /**
@@ -11,14 +12,39 @@ set_include_path(implode(PATH_SEPARATOR, array(INST_PATH . 'vendors', INST_PATH 
 include("GeneralCore.php");
  /**
  * Archivo {@link ActiveRecord.php}
- * 
+ *
  * Archivo que contiene la clase de Active Records, requerido para poder cargar todos los objetos.
  */
-include("ActiveRecord.php");
-				 
+require_once "ActiveRecord.php";
+
 include("Page.php");
 
 require 'Dispatcher.php';
 
 $index = new index();
+$xhprof_data = xhprof_disable();
+
+//
+// Saving the XHProf run
+// using the default implementation of iXHProfRuns.
+//
+include_once "/home/rantes/sites/xhprof/xhprof_lib/utils/xhprof_lib.php";
+include_once "/home/rantes/sites/xhprof/xhprof_lib/utils/xhprof_runs.php";
+
+$xhprof_runs = new XHProfRuns_Default();
+
+// Save the run under a namespace "xhprof_foo".
+//
+// **NOTE**:
+// By default save_run() will automatically generate a unique
+// run id for you. [You can override that behavior by passing
+// a run id (optional arg) to the save_run() method instead.]
+//
+$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_foo");
+
+echo "---------------\n".
+		"Assuming you have set up the http based UI for \n".
+		"XHProf at some address, you can view run at \n".
+		"http://localhost/xhprof/xhprof_html/index.php?run=$run_id&source=xhprof_foo\n".
+		"---------------\n";
 ?>
