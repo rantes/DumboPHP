c81b11b61b793dff3edcb538a8b3c0c51f12ad4b
Javier Serrano <rantes.javier@gmail.com>
2015-06-30 15:40:10 -0500

    Fixed update install

--- install.php
+++ install.php
@@ -8,31 +8,36 @@ $pathSrc = $path.'/src';
 $binPath = '/usr/bin';
 
 echo 'Installing DumboPHP. Please be patient...'.PHP_EOL;
-if(!file_exists($dumboSystemPath)) {
-	mkdir($dumboSystemPath);
-	$d = dir($path);
-	while (false !== ($entry = $d->read())) {
-	   if($entry != '.' && $entry != '..' && $entry != 'install.php' && $entry != 'src'){
-	   		echo 'copying '.$entry.PHP_EOL;
-	   		copy($path.'/'.$entry, $dumboSystemPath.'/'.$entry);
-	   }
-	}
-	$d->close();
-
-	mkdir($dumboSystemPathSrc);
-	$d = dir($pathSrc);
-	while (false !== ($entry = $d->read())) {
-	   if($entry != '.' && $entry != '..'){
-	   		echo 'copying '.$entry.PHP_EOL;
-	   		copy($pathSrc.'/'.$entry, $dumboSystemPathSrc.'/'.$entry);
-	   }
-	}
-	$d->close();
-
-	echo 'Creating bin file.'.PHP_EOL;
-	symlink($dumboSystemPath.'/dumbo', $binPath.'/dumbo');
+if(file_exists($dumboSystemPath)){
+	echo 'Updating DumboPHP...',PHP_EOL;
 } else {
-	die('Install failed: DumboPHP is already installed in '.$dumboSystemPath.PHP_EOL);
+	mkdir($dumboSystemPath);
 }
 
+$d = dir($path);
+while (false !== ($entry = $d->read())) {
+   if($entry != '.' && $entry != '..' && $entry != 'install.php' && $entry != 'src' && $entry != '.git' && $entry != '.gitignore'){
+   		echo 'copying '.$entry.PHP_EOL;
+   		copy($path.'/'.$entry, $dumboSystemPath.'/'.$entry);
+   }
+}
+$d->close();
+
+if(!file_exists($dumboSystemPath)){
+	mkdir($dumboSystemPathSrc);
+}
+
+$d = dir($pathSrc);
+while (false !== ($entry = $d->read())) {
+   if($entry != '.' && $entry != '..'){
+   		echo 'copying '.$entry.PHP_EOL;
+   		copy($pathSrc.'/'.$entry, $dumboSystemPathSrc.'/'.$entry);
+   }
+}
+$d->close();
+
+echo 'Creating bin file.'.PHP_EOL;
+file_exists($dumboSystemPath.'/dumbo') or symlink($dumboSystemPath.'/dumbo', $binPath.'/dumbo');
+
+echo 'Install complete.'.PHP_EOL;
 ?>
