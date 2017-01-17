bb2c88e1a563845d74b70bd70e033be6ff386442
Javier Serrano <rantes.javier@gmail.com>
2017-01-17 03:03:22 -0500

    Creating unit testing shell

---
+++ lib/Timothy/Shell.php
@@ -0,0 +1,13 @@
+<?php
+class TimothyShell {
+    private $commands = array(
+            'create',
+            'run',
+            'init',
+            'db',
+            'generate',
+            'destroy',
+            'migration'
+    );
+}
+?>
