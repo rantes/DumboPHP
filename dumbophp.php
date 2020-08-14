3324730590bf449e4bb5c22fc90531a62b69af5b
Javier Serrano <rantes.javier@gmail.com>
2020-08-14 09:29:44 -0500

    hotfix on model validations

--- dumbophp.php
+++ dumbophp.php
@@ -643,6 +643,7 @@ class Connection extends PDO {
             empty($this->_settings['password']) and $this->_settings['password'] = null;
             parent::__construct($dsn, $this->_settings['username'], $this->_settings['password'], [PDO::ATTR_PERSISTENT => true]);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         } catch (PDOException $e) {
             die('Error to connect to database due to: '.$e->getMessage());
         } catch (Exception $e) {
@@ -983,13 +984,16 @@ abstract class ActiveRecord extends Core_General_Class implements JsonSerializab
         } catch (PDOException $e) {
             throw new Exception("Failed to run {$this->_sqlQuery} due to: {$e->getMessage()}");
         }
+
+        $sh->setFetchMode(PDO::FETCH_CLASS, get_class($obj));
+        $resultset = $sh->fetchAll();
+
         $cols = $sh->columnCount();
         for ($i = 0; $i < $cols; $i++) {
             $meta = $sh->getColumnMeta($i);
             $obj->_set_columns($meta);
         }
-        $sh->setFetchMode(PDO::FETCH_CLASS, get_class($obj));
-        $resultset = $sh->fetchAll();
+
         $obj->_counter = $GLOBALS['Connection']->engine === 'sqlite' ? sizeof($resultset) : $sh->rowCount();
         $sh->closeCursor();
         $obj->exchangeArray($resultset);
@@ -1203,10 +1207,9 @@ abstract class ActiveRecord extends Core_General_Class implements JsonSerializab
                     }
                     if (!empty($this->{$field['field']})) {
                         $obj1 = new $this;
-                        $pk = empty($this->{$this->pk}) ? 0 : $this->{$this->pk};
                         $resultset = $obj1->Find([
                             'fields' => $field['field'], 
-                            'conditions' => "`{$field['field']}`='" .$this->{$field['field']} ."' AND `{$this->pk}`<>'{$pk}'"
+                            'conditions' => "`{$field['field']}`='" .$this->{$field['field']} ."' AND `{$this->pk}`<>'" .$this->{$this->pk} ."'"
                         ]);
                         $resultset->counter() > 0 && $this->_error->add(['field' => $field['field'], 'message' => $message]);
                     }
